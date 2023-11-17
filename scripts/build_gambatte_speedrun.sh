b2d7263175a9ed3f39fac229990d3a185b795082
Shivansh Srivastava <95683373+DarkSamus669@users.noreply.github.com>
2023-11-18 01:39:57 +0530

    Update build_gambatte_speedrun.sh

--- scripts/build_gambatte_speedrun.sh
+++ scripts/build_gambatte_speedrun.sh
@@ -4,4 +4,4 @@
 wget -q "https://github.com/pokemon-speedrunning/gambatte-speedrun/archive/refs/tags/r717.tar.gz" \
 	 -O gambatte-speedrun.tar.gz && tar -xf gambatte-speedrun.tar.gz
 
-cd gambatte-speedrun-r717 && sh scripts/build_qt
+cd gambatte-speedrun-r717 && sh scripts/build_qt.sh
